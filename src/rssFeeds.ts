f672a6d1c5db578bef755e23023d64e869a74933
peytonhobson <peyton.hobson1@gmail.com>
2024-08-23 15:09:48 -0600

    Additional feeds

--- src/rssFeeds.ts
+++ src/rssFeeds.ts
@@ -4,5 +4,11 @@ export const rssFeeds = [
   'https://maps.org/feed/',
   'https://www.sciencedaily.com/rss/mind_brain/psychedelic_drugs.xml',
   'https://psychedelicreview.com/feed/',
-  'https://hopkinspsychedelic.org/index?format=rss'
+  'https://hopkinspsychedelic.org/index?format=rss',
+  'https://doubleblindmag.com/feed/',
+  'https://thethirdwave.co/feed/',
+  'https://www.lucid.news/feed/',
+  'https://microdosinginstitute.com/feed/',
+  'https://chacruna.net/feed/',
+  'https://www.psymposia.com/feed/'
 ] as const
