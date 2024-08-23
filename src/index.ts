b857c1e8100f0703dab2d3822e9fea18374fa0ac
peytonhobson <peyton.hobson1@gmail.com>
2024-08-23 14:52:54 -0600

    remove unecessary creds

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,6 @@ import { tweetArticle } from './tweetArticle'
 
 config()
 
-const { value: credentials, hasError } = runSafe(() => ({
-  appKey: r.required(r.string(process.env.TWITTER_API_KEY)),
-  appSecret: r.required(r.string(process.env.TWITTER_API_SECRET)),
-  accessToken: r.required(r.string(process.env.TWITTER_ACCESS_TOKEN)),
-  accessSecret: r.required(r.string(process.env.TWITTER_ACCESS_SECRET))
-}))
-
-if (hasError) {
-  console.error('Error loading credentials:', credentials)
-  process.exit(1)
-}
-
 run(async () => {
   const db = await connectDB()
 
