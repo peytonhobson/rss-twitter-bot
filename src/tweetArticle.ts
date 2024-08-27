7c01b64eec70b470737554d46de313ffacbf822a
peytonhobson <peyton.hobson1@gmail.com>
2024-08-27 14:52:05 -0600

    Try with bitly

--- src/tweetArticle.ts
+++ src/tweetArticle.ts
@@ -1,13 +1,17 @@
+import { run } from '@crossingminds/utils'
 import { OpenAI } from 'openai'
 import { twitterClient } from './twitterClient'
+import { shortenUrl } from './shortenUrl'
 import type { FeedItem } from './fetchArticles'
 
-function getPrompt(article: FeedItem) {
+async function getPrompt(article: FeedItem) {
+  const shortenedLink = await shortenUrl(article.link)
+
   return `
-  You are a social media manager for a Twitter account focused on psychedelics. Create a unique, engaging tweet about the following article snippet. Avoid using too many emojis. Make sure the tweet is concise, informative, and includes a call to action for readers to learn more. Also include relevant formatting, such as new lines and hashtags.
+  You are a social media manager for a Twitter account focused on psychedelics. Create a unique, engaging tweet about the following article snippet. Avoid using too many emojis. Make sure the tweet is concise, informative, and includes a call to action for readers to learn more. Also include relevant formatting, such as new lines and hashtags. Make sure total characters do not exceed 280.
 
   Article Title: "${article.title}"
-  Article Link: "${article.link}"
+  Article Link: "${shortenedLink}"
   Article Twitter Handle: "${article.twitterHandle}"
   Snippet: "${article.contentSnippet.slice(0, 150)}"
   `
@@ -18,16 +22,28 @@ const openaiClient = new OpenAI({
 })
 
 export async function tweetArticle(article: FeedItem) {
+  const content = await getPrompt(article)
+
   const response = await openaiClient.chat.completions.create({
-    messages: [{ role: 'user', content: getPrompt(article) }],
-    model: 'gpt-4o-mini'
+    messages: [{ role: 'user', content }],
+    model: 'gpt-4o-mini',
+    max_tokens: 70
   })
 
   const tweet = response.choices[0]?.message?.content?.trim()
 
+  // Ensure finalTweet is within 280 characters
+  const finalTweet = run(() => {
+    if (tweet.length > 280) {
+      return `${tweet.substring(0, 277)}...`
+    }
+
+    return tweet
+  })
+
   try {
-    await twitterClient.v2.tweet(tweet)
-    console.log(`Tweeted: ${tweet}`)
+    await twitterClient.v2.tweet(finalTweet)
+    console.log(`Tweeted: ${finalTweet}`)
   } catch (error) {
     console.error('Error posting tweet:', error)
     process.exit(1)
