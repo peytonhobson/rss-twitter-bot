28ffe52d51b23d9bcbf328cccf1172c5d2fb0f57
peytonhobson <peyton.hobson1@gmail.com>
2024-08-27 19:10:23 -0600

    Try without length limit

--- src/tweetArticle.ts
+++ src/tweetArticle.ts
@@ -12,17 +12,17 @@ async function getPrompt(article: FeedItem) {
   const chosenTone = toneOptions[Math.floor(Math.random() * toneOptions.length)]
 
   return `
-  You are a social media manager for a Twitter account focused on psychedelics. ${chosenTone} Create a unique tweet about the following article snippet, but do not include the link or source information. Avoid using too many emojis. Make sure the tweet is concise, informative, and includes a call to action for readers to learn more. Also include relevant formatting, such as new lines and hashtags. Keep the tweet under 200 characters.
+  You are a social media manager for a Twitter account focused on psychedelics. ${chosenTone} Create a unique tweet about the following article snippet, but do not include the link or source information. Avoid using too many emojis. Make sure the tweet is concise, informative, and includes a call to action for readers to learn more. Also include relevant formatting, such as new lines and hashtags.
 
   Article Title: "${article.title}"
+  Article Link: "${article.link}"
+  Article Twitter Handle: "${article.twitterHandle}"
   Snippet: "${article.contentSnippet.slice(0, 150)}"
   `
 }
 
-const MAX_TWEET_LENGTH = 320
-
 const openaiClient = new OpenAI({
-  apiKey: process.env['OPEN_AI_KEY'] // This is the default and can be omitted
+  apiKey: process.env['OPEN_AI_KEY']
 })
 
 export async function tweetArticle(article: FeedItem) {
@@ -33,33 +33,7 @@ export async function tweetArticle(article: FeedItem) {
     model: 'gpt-4o-mini'
   })
 
-  const handleOrSource = article.twitterHandle
-    ? `@${article.twitterHandle}`
-    : new URL(article.link).hostname.replace('www.', '')
-
-  // Define static content
-  const staticContent = `\n\nSource: ${handleOrSource}\n\nRead more: ${article.link}`
-
-  let dynamicContent = response.choices[0]?.message?.content?.trim() || ''
-  let tweet = `${dynamicContent}${staticContent}`
-
-  // If the tweet is too long, trim the last sentence(s) of the dynamic content
-  if (tweet.length > MAX_TWEET_LENGTH) {
-    while (tweet.length > MAX_TWEET_LENGTH && dynamicContent.includes('.')) {
-      const lastSentenceIndex = dynamicContent.lastIndexOf('.')
-      dynamicContent = dynamicContent.substring(0, lastSentenceIndex + 1)
-      tweet = `${dynamicContent}${staticContent}`
-    }
-
-    // If after trimming sentences it's still too long, throw
-    if (tweet.length > MAX_TWEET_LENGTH) {
-      console.error('Tweet is too long:', tweet)
-      process.exit(1)
-    }
-  }
-
-  console.log('Generated tweet:', tweet)
-  console.log('Tweet length:', tweet.length)
+  const tweet = response.choices[0].message.content.trim()
 
   try {
     await twitterClient.v2.tweet(tweet)
