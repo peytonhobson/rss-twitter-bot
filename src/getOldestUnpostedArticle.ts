d3f432250c145008f2f27d88e0a8a861c81464d7
peytonhobson <peyton.hobson1@gmail.com>
2024-08-29 07:36:42 -0600

    not same author

--- src/getOldestUnpostedArticle.ts
+++ src/getOldestUnpostedArticle.ts
@@ -21,6 +21,33 @@ export async function getOldestUnpostedArticle(db: Db, articles: FeedItem[]) {
 
   let oldestUnpostedArticle = undefined
 
+  const lastPostedArticle = await db
+    .collection('postedArticles')
+    .find()
+    .sort({ _id: -1 })
+    .limit(1)
+    .toArray()
+    .then(arr => arr[0])
+
+  if (lastPostedArticle) {
+    const lastPostedArticleAuthor = new URL(lastPostedArticle.link).hostname
+
+    const articlesByOtherAuthors = filteredArticles.filter(
+      ({ link }) => new URL(link).hostname !== lastPostedArticleAuthor
+    )
+
+    for (const article of articlesByOtherAuthors) {
+      if (!(await isArticlePosted(db, article.link))) {
+        oldestUnpostedArticle = article
+        break
+      }
+    }
+  }
+
+  if (oldestUnpostedArticle) {
+    return oldestUnpostedArticle
+  }
+
   for (const article of filteredArticles) {
     if (!(await isArticlePosted(db, article.link))) {
       oldestUnpostedArticle = article
