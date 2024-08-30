ae369cbebf706a17738c3fcde7bae58fb225bc58
peytonhobson <peyton.hobson1@gmail.com>
2024-08-30 11:24:03 -0600

    prompt update

--- src/tweetArticle.ts
+++ src/tweetArticle.ts
@@ -12,7 +12,7 @@ async function getPrompt(article: FeedItem) {
   const chosenTone = toneOptions[Math.floor(Math.random() * toneOptions.length)]
 
   return `
-  You are a social media manager for a Twitter account focused on psychedelics. ${chosenTone} Create a unique tweet about the following article snippet. Use the template and example provided below to structure the tweet. Ensure the article link appears right after the headline so that the associated image is previewed with the tweet. Avoid using emojis. Make sure the tweet is concise, informative, and includes a call to action for readers to learn more. Include relevant formatting, such as new lines and hashtags.
+  You are a social media manager for a Twitter account focused on psychedelics. ${chosenTone} Create a unique tweet about the following article snippet. Use the template and example provided below to structure the tweet. Ensure the article link appears right after the headline so that the associated image is previewed with the tweet. Avoid using emojis. Make sure the tweet is concise, informative, and includes a call to action for readers to learn more. Add new lines at the end of each paragraph. Add hashtags at the end of the tweet.
 
   ### Template:
   {headline}
@@ -21,7 +21,8 @@ async function getPrompt(article: FeedItem) {
   {call_to_action}
 
   ### Example:
-  MDMA Therapy: The Future of PTSD Treatment is Here \n
+
+  MDMA Therapy: The Future of PTSD Treatment is Here
 
   Source: @psyedelics https://example.com/article
 
