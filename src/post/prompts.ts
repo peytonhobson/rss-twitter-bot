c5f5bc0c30812a3e882c91aeb3a5329ea4abac87
peytonhobson <peyton.hobson1@gmail.com>
2024-09-03 16:53:58 -0600

    rip the threads

--- src/post/prompts.ts
+++ src/post/prompts.ts
@@ -1,7 +1,6 @@
-import { openaiClient } from '../openaiClient'
 import type { FeedItem } from './fetchArticles'
 
-export async function getRegularTweet(article: FeedItem) {
+export async function getRegularPrompt(article: FeedItem) {
   const toneOptions = [
     'Write in a friendly, engaging tone.',
     'Write in a concise and informative tone.',
@@ -10,7 +9,7 @@ export async function getRegularTweet(article: FeedItem) {
 
   const chosenTone = toneOptions[Math.floor(Math.random() * toneOptions.length)]
 
-  const content = `
+  return `
   You are a social media manager for a Twitter account focused on psychedelics. ${chosenTone} Create a unique tweet about the following article snippet. Use the template and example provided below to structure the tweet. Avoid using emojis. Make sure the tweet is concise, informative, and includes a call to action for readers to learn more. Add new lines at the end of each paragraph. Add hashtags at the end of the tweet.
 
   ### Template:
@@ -31,34 +30,13 @@ export async function getRegularTweet(article: FeedItem) {
   Article Twitter Handle: "${article.twitterHandle || ''}"
   Snippet: "${article.contentSnippet.slice(0, 150)}"
   `
-
-  const response = await openaiClient.chat.completions.create({
-    messages: [{ role: 'user', content }],
-    model: 'gpt-4o'
-  })
-
-  return response.choices[0].message.content.trim()
 }
 
-export async function getThreadTweet(article: FeedItem) {
-  const content = `
+export async function getThreadPrompt(article: FeedItem) {
+  return `
   You are an expert in psychedelics and wellness. Break down the following article into a series of tweets (up to 5 tweets) that summarize the key points or findings of the article. Each tweet should be engaging and provide valye.  Add new lines at the end of each paragraph. Don't use emojis. There should only be one question in the thread and it should be at the end of the last tweet. The question should be thought-provoking and encourage readers to think more about the article. Only include the article link at the end of the last tweet.
 
   Article Title: "${article.title}"
   Snippet: "${article.content.slice(0, 2000)}"
-  `
-
-  const response = await openaiClient.chat.completions.create({
-    messages: [{ role: 'user', content }],
-    model: 'gpt-4o'
-  })
-
-  const tweet = response.choices[0].message.content.trim()
-
-  return `
-  Source: ${article.link}
-  ${article.twitterHandle ? `From: @${article.twitterHandle}` : ''}
-
-  ${tweet}
   `
 }
--- src/post/tweetArticle.ts
+++ src/post/tweetArticle.ts
@@ -1,17 +1,54 @@
 import { twitterClient } from '../twitterClient'
-import { getRegularTweet, getThreadTweet } from './prompts'
+import { openaiClient } from '../openaiClient'
+import { getRegularPrompt, getThreadPrompt } from './prompts'
 import type { FeedItem } from './fetchArticles'
 
 export async function tweetArticle(article: FeedItem) {
   const shouldCreateThread = isArticleSmallEnoughForThread(article)
 
-  const tweet = await getRegularTweet(article)
+  if (shouldCreateThread) {
+    await createTwitterThread(article)
+  } else {
+    await createRegularTweet(article)
+  }
+}
+
+async function createRegularTweet(article: FeedItem) {
+  const tweet = await getRegularPrompt(article)
 
   try {
     await twitterClient.v2.tweet(tweet)
     console.log(`Tweeted: ${tweet}`)
   } catch (error) {
     console.error('Error posting tweet:', error)
+    process.exit(1)
+  }
+}
+async function createTwitterThread(article: FeedItem) {
+  const content = await getThreadPrompt(article)
+
+  const response = await openaiClient.chat.completions.create({
+    model: 'gpt-4o',
+    messages: [{ role: 'user', content }]
+  })
+
+  const generatedContent = response.choices[0]?.message?.content?.trim() || ''
+
+  const tweets = generatedContent
+    .split(/(?=\n\d+\/\d+)/)
+    .map(tweet => tweet.trim())
+
+  tweets[0] = `
+  ${tweets[0]}
+
+  Source: ${article.link}
+  ${article.twitterHandle ? `From: @${article.twitterHandle}` : ''}
+  `
+
+  try {
+    await twitterClient.v2.tweetThread(tweets)
+  } catch (error) {
+    console.error('Error posting thread:', error)
     process.exit(1)
   }
 }
